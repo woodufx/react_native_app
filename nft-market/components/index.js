f275bc63b832ff0874281c6f53d6122ce8b78a21
Dmitry Limansky <DIMCH.L@mail.ru>
2023-06-07 23:50:21 +0300

    Finished first react native app

---
+++ nft-market/components/index.js
@@ -0,0 +1,13 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo, EthPrice, NFTTitle, ImgaeCmp, People, EndDate } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+  HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton,
+  SubInfo, EthPrice, NFTTitle, ImgaeCmp, People, EndDate,
+  DetailsDesc, DetailsBid
+};
